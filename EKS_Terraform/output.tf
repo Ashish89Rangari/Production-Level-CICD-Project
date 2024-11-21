b5286ac918017d8abfd7393dd3ced66a53217af4
ASHISH-V-RANGARI <150202892+Ashish89Rangari@users.noreply.github.com>
2024-11-21 14:44:58 +0900

    Updated EKS cluster files

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,17 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.ashu.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.ashu.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.ashu_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.ashu_subnet[*].id
 }
- 
- 
